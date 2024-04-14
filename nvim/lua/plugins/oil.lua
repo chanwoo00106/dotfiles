3f39396673c5a89d0c8c853e4fe8a1cc392782c3
변찬우 <byun00106@gmail.com>
2024-04-14 21:23:23 +0900

    feat: add oil git status plugin

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -10,9 +10,19 @@ return {
         ["<C-h>"] = "<Cmd>wincmd h<CR>",
         ["H"] = "actions.select_split",
       },
+      win_options = {
+        signcolumn = "yes:2",
+      },
     },
     config = function(_, opts)
       require("oil").setup(opts)
     end,
+  },
+  {
+    "refractalize/oil-git-status.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    config = true,
   },
 }
