5207969ec78a506503738f06e80ce6093fca69ef
변찬우 <byun00106@gmail.com>
2024-04-20 13:25:09 +0900

    refactor: __check_nvm move config.fish to functions

--- fish/config.fish
+++ fish/config.fish
@@ -10,19 +10,6 @@ end
 alias sd "cd ~ && cd (find * -type d | fzf)"
 
 # nvm setup
-function __check_nvm --on-variable PWD --description 'Do nvm stuff'
-    if test -f .nvmrc
-        set node_version_target (cat .nvmrc)
-        set nvmrc_node_version (nvm list | grep $node_version_target)
-
-        if not test -n "$nvmrc_node_version"
-            # install
-            nvm install $node_version_target
-        else
-            nvm use $node_version_target
-        end
-    end
-end
 __check_nvm
 
 # starship setup
