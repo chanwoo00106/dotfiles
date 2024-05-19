fd18af2303ed1f19d0e2b84fc0010a51f29c062b
변찬우 <byun00106@gmail.com>
2024-05-19 21:15:51 +0900

    fix: remove wezterm tabbar

---
+++ wezterm/tabbar.lua
@@ -0,0 +1,7 @@
+local module = {}
+
+function module.apply_to_config(config)
+	config.hide_tab_bar_if_only_one_tab = true
+end
+
+return module
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local helpers = require("helpers")
+local tabbar = require("tabbar")
 
 local config = {}
 
 helpers.apply_to_config(config)
+tabbar.apply_to_config(config)
 
 return config
