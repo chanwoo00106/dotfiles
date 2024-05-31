0bbb91340d4fe396b7f0371e012ebe60897c2282
변찬우 <byun00106@gmail.com>
2024-05-31 10:09:43 +0900

    fix: disabled barbar plugin

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,7 +1,8 @@
 return {
-  { "akinsho/bufferline.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = true },
   {
     "romgrk/barbar.nvim",
+    enabled = false,
     dependencies = {
       "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
       "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
