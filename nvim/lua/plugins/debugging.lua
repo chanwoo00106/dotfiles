c8b3858377dffb3a2c14bdd8b5ccb46bd57e9c80
변찬우 <byun00106@gmail.com>
2024-06-24 10:43:43 +0900

    feat: add react debugging tool

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -4,6 +4,14 @@ return {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
+
+		-- Js
+		"mxsdev/nvim-dap-vscode-js",
+		{
+			"microsoft/vscode-js-debug",
+			version = "1.x",
+			build = "npm i && npm run compile vsDebugServerBundle && mv dist out",
+		},
 	},
 	config = function()
 		local dap, dapui = require("dap"), require("dapui")
@@ -15,12 +23,58 @@ return {
 		dap.listeners.before.launch.dapui_config = function()
 			dapui.open()
 		end
-		-- dap.listeners.before.event_terminated.dapui_config = function()
-		-- 	dapui.close()
-		-- end
-		-- dap.listeners.before.event_exited.dapui_config = function()
-		-- 	dapui.close()
-		-- end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		dapui.setup()
+
+		dap.adapters.language = function(cb, config)
+			if config.request == "attach" then
+				cb({ type = "server", port = 9222 })
+			else
+				if config.request == "launch" then
+					cb({ type = "executable", command = "path/to/executable" })
+				end
+			end
+		end
+
+		require("dap-vscode-js").setup({
+			debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+			adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+		})
+
+		local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+		for _, language in ipairs(js_based_languages) do
+			require("dap").configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "attach",
+					name = "Attach",
+					processId = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-chrome",
+					request = "launch",
+					name = 'Start Chrome with "localhost"',
+					url = "http://localhost:3000",
+					webRoot = "${workspaceFolder}",
+					userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+				},
+			}
+		end
 
 		-- keymap
 		vim.keymap.set("n", "<leader>dc", function()
