adf316c51cae0c96b68f2b508894833c889f24d7
변찬우 <byun00106@gmail.com>
2024-06-24 16:25:23 +0900

    fix: change notice option

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,7 +2,42 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
-		-- add any options here
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 5,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = "auto",
+				},
+			},
+			popupmenu = {
+				relative = "editor",
+				position = {
+					row = 8,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = 10,
+				},
+				border = {
+					style = "rounded",
+					padding = { 0, 1 },
+				},
+				win_options = {
+					winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+				},
+			},
+		},
+
+		presets = {
+			bottom_search = true,
+			command_palette = true,
+			long_message_to_split = true,
+		},
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
